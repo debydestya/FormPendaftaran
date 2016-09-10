7758e09a9c8c9a3a7f6c82f40a353ef556154a47
DEBY <debydestyananda5c@gmail.com>
2016-09-10 22:44:11 +0700

    Membuat layout +script untuk edit text nama dan menampilkan hasil ke viewtextnama + dependencies

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2006/pendaftaranbimbel/MainActivity.java
@@ -0,0 +1,57 @@
+package id.sch.smktelkom_mlg.tugas01.xirpl2006.pendaftaranbimbel;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etNama;
+    TextView tvNama;
+    Button bOK;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        tvNama = (TextView) findViewById(R.id.textViewNama);
+        bOK = (Button) findViewById(R.id.buttonOK);
+
+        bOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doProcess();
+            }
+        });
+
+    }
+
+    private void doProcess() {
+        if (isValid()) {
+            String nama = etNama.getText().toString();
+            tvNama.setText("Nama    : " + nama);
+        }
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+
+        String nama = etNama.getText().toString();
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama belum diisi");
+            valid = false;
+        } else if (nama.length() < 3) {
+            etNama.setError("Nama minimal 3 karakter");
+            valid = false;
+        } else {
+            etNama.setError(null);
+        }
+
+        return valid;
+    }
+}
