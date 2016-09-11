8164b3115ca5c62c96f143c5cc8c8b7964feb6a0
DEBY <debydestyananda5c@gmail.com>
2016-09-11 16:45:41 +0700

    +TextView dua menampilkan pilihan kelas bimbel pada RadioButton +Muncul editText untuk menginputkan jumlah siswa jika user memilih tipe bimbel Kelompok +Efek respon perubahan warna jika button OK di klik

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2006/pendaftaranbimbel/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2006/pendaftaranbimbel/MainActivity.java
@@ -5,12 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText etNama;
-    TextView tvNama;
+    TextView tvNama, tvTipe;
+    RadioGroup rgTipe;
     Button bOK;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +22,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         etNama = (EditText) findViewById(R.id.editTextNama);
+        rgTipe = (RadioGroup) findViewById(R.id.radioGroupTipe);
         tvNama = (TextView) findViewById(R.id.textViewNama);
+        tvTipe = (TextView) findViewById(R.id.textViewTipe);
         bOK = (Button) findViewById(R.id.buttonOK);
 
         bOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 doProcess();
+                doProcess2();
             }
         });
 
+        rgTipe.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int i) {
+                if (i == R.id.radioButtonP) {
+                    findViewById(R.id.editTextJumlah).setVisibility(View.GONE);
+                } else {
+                    findViewById(R.id.editTextJumlah).setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+    }
+
+    private void doProcess2() {
+        String hasil = null;
+
+        if (rgTipe.getCheckedRadioButtonId() != -1) {
+            RadioButton rb = (RadioButton)
+                    findViewById(rgTipe.getCheckedRadioButtonId());
+            hasil = rb.getText().toString();
+
+            if (rgTipe.getCheckedRadioButtonId() != R.id.radioButtonP) {
+                EditText etJS = (EditText) findViewById(R.id.editTextJumlah);
+                hasil += "\nJumlah Siswa    : " + etJS.getText();
+            }
+        }
+
+        if (hasil == null) {
+            tvTipe.setText("Anda belum memilih tipe bimbel");
+        } else {
+            tvTipe.setText("Tipe Bimbel : " + hasil);
+        }
     }
 
     private void doProcess() {
