ba30e54cecce468f5692e8c3cff6b8522d0ce69d
DEBY <debydestyananda5c@gmail.com>
2016-09-11 17:53:53 +0700

    +TextViewPel menampilkan jumlah mata pelajaran (radio button) yang dipilih user +TextViewMapel menampilkan nama mata pelajaran yang telah dipilih user

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2006/pendaftaranbimbel/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2006/pendaftaranbimbel/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -15,12 +17,15 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
     EditText etNama;
-    TextView tvNama, tvTipe, tvKelas;
+    TextView tvNama, tvTipe, tvKelas, tvMapel, tvPel;
     RadioGroup rgTipe;
     Spinner spS, spK;
+    CheckBox cbM, cbI, cbB;
     Button bOK;
+
+    int nMapel;
 
     Integer[][] arKelas = {{1, 2, 3, 4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
     ArrayList<Integer> listKelas = new ArrayList<>();
@@ -36,14 +41,23 @@ public class MainActivity extends AppCompatActivity {
         rgTipe = (RadioGroup) findViewById(R.id.radioGroupTipe);
         spS = (Spinner) findViewById(R.id.spinnerSekolah);
         spK = (Spinner) findViewById(R.id.spinnerKelas);
+        cbM = (CheckBox) findViewById(R.id.checkBoxMat);
+        cbI = (CheckBox) findViewById(R.id.checkBoxIpa);
+        cbB = (CheckBox) findViewById(R.id.checkBoxBahasa);
+        tvPel = (TextView) findViewById(R.id.textViewPel);
         tvNama = (TextView) findViewById(R.id.textViewNama);
         tvTipe = (TextView) findViewById(R.id.textViewTipe);
         tvKelas = (TextView) findViewById(R.id.textViewKelas);
+        tvMapel = (TextView) findViewById(R.id.textViewMapel);
         bOK = (Button) findViewById(R.id.buttonOK);
 
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listKelas);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spK.setAdapter(adapter);
+
+        cbM.setOnCheckedChangeListener(this);
+        cbI.setOnCheckedChangeListener(this);
+        cbB.setOnCheckedChangeListener(this);
 
         bOK.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 doProcess();
                 doProcess2();
                 doProcess3();
+                doProcess4();
             }
         });
 
@@ -82,8 +97,20 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void doProcess4() {
+        String hasil = "Mata Pelajaran  :\n";
+        int startlen = hasil.length();
+        if (cbB.isChecked()) hasil += cbB.getText() + "\n";
+        if (cbI.isChecked()) hasil += cbI.getText() + "\n";
+        if (cbM.isChecked()) hasil += cbM.getText() + "\n";
+
+        if (hasil.length() == startlen) hasil += "Anda belum memilih Mata Pelajaran";
+
+        tvMapel.setText(hasil);
+    }
+
     private void doProcess3() {
-        tvKelas.setText("Sekolah    : " + spS.getSelectedItem().toString()
+        tvKelas.setText("Sekolah               : " + spS.getSelectedItem().toString()
                 + " Kelas " + spK.getSelectedItem().toString());
     }
 
@@ -112,14 +139,14 @@ public class MainActivity extends AppCompatActivity {
         if (hasil == null) {
             tvTipe.setText("Anda belum memilih tipe bimbel");
         } else {
-            tvTipe.setText("Tipe Bimbel : " + hasil);
+            tvTipe.setText("Tipe Bimbel        : " + hasil);
         }
     }
 
     private void doProcess() {
         if (isValid()) {
             String nama = etNama.getText().toString();
-            tvNama.setText("Nama    : " + nama);
+            tvNama.setText("Nama                  : " + nama);
         }
     }
 
@@ -139,5 +166,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return valid;
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) nMapel += 1;
+        else nMapel -= 1;
+
+        tvPel.setText("Mata Pelajaran (" + nMapel + " terpilih)");
     }
 }
